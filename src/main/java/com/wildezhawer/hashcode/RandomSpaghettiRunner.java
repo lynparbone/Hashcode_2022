4868adeb13a47e9d90ba255e81ac53f1ca881237
Bonelli, Lyndsey <lyndsey.bonelli@zuehlke.com>
2022-04-19 00:44:15 +0200

    Remove simulation, add some notes.

--- src/main/java/com/wildezhawer/hashcode/RandomSpaghettiRunner.java
+++ src/main/java/com/wildezhawer/hashcode/RandomSpaghettiRunner.java
@@ -1,15 +1,10 @@
 package com.wildezhawer.hashcode;
 
-import com.wildezhawer.hashcode.model.PlannedProject;
-import com.wildezhawer.hashcode.model.Project;
 import com.wildezhawer.hashcode.service.DataStore;
 import com.wildezhawer.hashcode.service.FileReaderService;
 import com.wildezhawer.hashcode.service.FileWriterService;
-import com.wildezhawer.hashcode.service.SimulationRunner;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.wildezhawer.hashcode.config.SimulationConfiguration.FILES_TO_READ;
 import static com.wildezhawer.hashcode.service.ProjectSorter.sortProjectsByDurationAscending;
@@ -25,26 +20,7 @@ public class RandomSpaghettiRunner {
             read(filename);
             DataStore.projects = sortProjectsByDurationAscending(DataStore.projects);
 
-            // Run simulation, i.e. plan the projects
-            SimulationRunner simulationRunner = new SimulationRunner(DataStore.projects, DataStore.contributors);
-
-            simulationRunner.run();
-
-            // Retrieve all the completed projects
-            List<Project> completedProjects = simulationRunner.projects.stream()
-                    .filter(project -> simulationRunner.allProjectStatus.get(project.getName()) == SimulationRunner.ProjectStatus.COMPLETED)
-                    .collect(Collectors.toList());
-
-            // Setup results for correct output format
-            List<PlannedProject> plannedProjects = new ArrayList<>();
-            for (Project project : completedProjects) {
-                PlannedProject plannedProject = new PlannedProject();
-                plannedProject.setProject(project.getName());
-                plannedProject.setContributors(simulationRunner.projectStaffing.get(project.getName()));
-                plannedProjects.add(plannedProject);
-            }
-
-            DataStore.plannedProjects = plannedProjects;
+            // TODO: Run simulation, i.e. plan the projects
 
             // Write the output files in the correct format
             write(filename);
